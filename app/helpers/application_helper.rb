49cf9dd95e9c370784a6668b31eb2e6f952dd905
Nerbyk <danil.maximov2000@gmail.com>
2020-08-27 19:49:13 +0200

    mobile version styling, default layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title="")
+        base_title = "Kids Club"
+        if page_title.empty?
+            base_title
+        else  
+            page_title + " | " + base_title
+        end
+    end
 end
